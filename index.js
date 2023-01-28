c3ad3cdca1e4d651a106cc9fc847864917a9df50
rlahfld54 <rlahfld54@naver.com>
2023-01-29 00:18:46 +0900

    fix : 오류 수정

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const mysql = require("mysql2");
 const dbconfig = require("./config/database.js");
 const connection = mysql.createConnection(dbconfig);
+// 암호화 라이브러리.. recommended
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 // const database = [
 //   {
 //     id: 1,
@@ -21,6 +25,8 @@ const connection = mysql.createConnection(dbconfig);
 // nodemon 설치했는데 제대로 작동되는지 확인할 것!!
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 connection.connect();
 
 // configuration =========================
@@ -30,9 +36,6 @@ app.set("port", process.env.PORT || 3000);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 // 로그인 기능은 카카오톡 로그인도 추가로 해볼 것!!
 
@@ -75,24 +78,41 @@ app.get("/login", (req, res) => {
 });
 
 // 회원가입
-app.post("/signup", (req, res) => {
+app.post("/signup", (req, res, next) => {
   // 클라이언트에서 받은 회원가입 데이터가 이미 저장되어있는지 확인해야한다.
   // 기존 정보들과 비교해서 중복 된 것이 없다면 삽입쿼리를 실행하고
   // 중복된 유저가 이미 있다면 중복된 유저가 있다고 클라이언트에 알려줘야한다.
   // 그런데 여기에는 중복체크는 안되어있다.
+  // 그리고 암호화를 해준다. crypto 모듈 설치해서 사용
   let users = req.body;
   console.log(users);
 
-  connection.query(
-    `INSERT INTO Users (userid,email,password,birthday,gender,createtime,updatetime)
-    VALUES ('${users.userId}','${users.email}','${users.password}','${users.birthday}','${users.gender}','${users.createtime}','${users.updatetime}')`,
-    (error, rows) => {
-      if (error) throw error;
-      // if (!error === null) console.log(error);
-      console.log("회원가입을 성공했습니다.");
-      res.send();
+  //비밀번호 암호화 하기
+  const hashPassword = bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(users.password, salt, function (err, hash) {
+      // Store hash in your password DB.
+      if (err) return next(err);
+      users.password = hash;
+      next();
+    });
+  });
+
+  var query = `SELECT userId FROM Users WHERE userId ='${users.userId}'`; // 중복 아이디 체크
+  connection.query(query, (error, rows) => {
+    if (error) throw error;
+    if (rows.length == 0) {
+      // sql제대로 연결되고 중복이 없는 경우
+      var query = `INSERT INTO Users (userid,email,password,birthday,gender,createtime,updatetime) VALUES ('${users.userId}','${users.email}','${hashPassword}','${users.birthday}','${users.gender}','${users.createtime}','${users.updatetime}')`;
+      connection.query(query, (error, rows) => {
+        if (error) throw error;
+        else {
+          console.log("회원가입을 성공했습니다.");
+          res.send("회원가입 성공");
+        }
+      });
     }
-  );
+  });
 });
 
 // CRUD
